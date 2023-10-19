38e54888f178bfc67656056d94ad7f3294f8d817
shivamjos1994 <shivamjos1994@gmail.com>
2023-10-19 14:24:27 +0530

    infinite scroll using django

--- myTasks/settings.py
+++ myTasks/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
     'task',
     'flights',
     'users',
+    'scroll',
 ]
 
 MIDDLEWARE = [
@@ -119,8 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=BASE_DIR/'static'
-STATICFILES_DIRS=['myTasks/static']
+STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'checkstatic/static/')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
